b72396408cd5cf077e9639fc4b00781c5072dfb3
AMOviedo15 <AMOviedo15@gmail.com>
2021-03-03 03:15:06 -0600

    finished

--- Zylabs6.17.py
+++ Zylabs6.17.py
@@ -1,1 +1,24 @@
+#Aaron Oviedo #1990958
 
+i = 0
+userpass = input()
+newpass = ""
+
+while i  < len(userpass):
+    index = userpass[i]
+    if index == 'i':
+        newpass += '!'
+    elif index == 'a':
+        newpass += '@'
+    elif index == 'm':
+        newpass += 'M'
+    elif index == 'B':
+        newpass += '8'
+    elif index == 'o':
+        newpass += '.'
+    else:
+        newpass += index
+    i += 1
+newpass += "q*s"
+
+print(newpass)
