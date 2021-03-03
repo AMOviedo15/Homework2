3bb6aece2cc95c27b419cd370ec5393e10be5172
AMOviedo15 <AMOviedo15@gmail.com>
2021-03-03 04:57:07 -0600

    finished

--- Zylabs8.10.py
+++ Zylabs8.10.py
@@ -1,1 +1,21 @@
+#Aaron Oviedo #1990958
 
+userval = input()
+i = 0
+j = len(userval)-1
+result = True
+while(i<j):
+    if(userval[i]==' '):
+        i+=1
+    elif(userval[j]==' '):
+        j-=1
+    elif(userval[i]!=userval[j]):
+        result = False
+        break
+    else:
+        i+=1
+        j-=1
+if (result):
+    print(userval,"is a palindrome")
+else:
+    print(userval, "is not a palindrome")
