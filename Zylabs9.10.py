eb26514bcb05a57d90af0f5de53aaa77b64aa4b9
AMOviedo15 <AMOviedo15@gmail.com>
2021-03-03 18:02:22 -0600

    finished

--- Zylabs9.10.py
+++ Zylabs9.10.py
@@ -1,1 +1,19 @@
+#Aaron Oviedo #1990958
 
+import csv
+
+userfile = input()
+
+wordNum = {}
+
+with open(userfile, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        for word in row:
+            if word not in wordNum.keys():
+                wordNum[word] = 1
+            else:
+                wordNum[word] += 1
+
+    for key in wordNum.keys():
+        print(key + " " + str(wordNum[key]))
